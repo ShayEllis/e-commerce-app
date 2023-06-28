741ff7704fe58ecdcdc400fbf1d0e686f0529e24
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-06-28 10:55:52 -0700

    added cores and body-parser

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from 'express'
-
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+// Create an express app and set port number.
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+// Import environment variables from .env file.
+dotenv.config()
+// Setup body-parser
+app.use(bodyParser.json())
+// Setup cors
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(process.env.HELLO_MESSAGE)
 })
 
 app.listen(port, () => {
